c0eaa6cc2172aa3d278f55063c70e90cb1acef54
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 15:17:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,12 @@ def roll_call_dwarves(dwarfs)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  veggies.collect do |call|
+    call.capitalize + "!"
+  end
+
 end
 
 def long_planeteer_calls# code an argument here
