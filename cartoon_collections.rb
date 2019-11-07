3a6f0740acbd0a4397eddb398cf92c60bacc1a7c
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 15:18:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,10 @@ def summon_captain_planet(veggies)# code an argument here
 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
+  answer = false
+  long_planeteer_calls.each do |call|
 end
 
 def find_the_cheese# code an argument here
