0fbdde5ef475414f7f3aed01c70248ed43de7afc
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 15:22:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,10 +16,10 @@ end
 def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
   answer = false
- long_planteer_calls.each do |call|
-   if call.length > 4
-     answer = true
-    end
+long_planteer_calls.each do |call|
+  if call.length > 4
+    answer = true
+  end
 end
   answer
 end
