d3c6dc4194c5c4ba59e308b741770bd79bd5d9d0
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 15:21:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,8 @@ def long_planeteer_calls(long_planeteer_calls)# code an argument here
      answer = true
     end
 end
+  answer
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
